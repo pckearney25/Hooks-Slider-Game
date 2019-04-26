0403e0cbdda900b4cb3f32be1658f4d812336ffe
Patrick Kearney <pckearney@yahoo.com>
2019-04-26 14:58:37 -0500

    build with Hooks

---
+++ build/precache-manifest.781fd26f8c07eaf2c798319bc11cb6a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2ac769bef0deda2d7aba9f923ad8f7b",
+    "url": "/Hooks-Slider-Game/index.html"
+  },
+  {
+    "revision": "10245b069936e7647e3d",
+    "url": "/Hooks-Slider-Game/static/css/main.223dfe48.chunk.css"
+  },
+  {
+    "revision": "1ed47eb8a12884a418eb",
+    "url": "/Hooks-Slider-Game/static/js/2.3db1a4b2.chunk.js"
+  },
+  {
+    "revision": "10245b069936e7647e3d",
+    "url": "/Hooks-Slider-Game/static/js/main.8f88106e.chunk.js"
+  },
+  {
+    "revision": "8293dcf0fdfc3a30eeb7",
+    "url": "/Hooks-Slider-Game/static/js/runtime~main.be4b318e.js"
+  },
+  {
+    "revision": "72570664c46adea25c7900c2c94a294f",
+    "url": "/Hooks-Slider-Game/static/media/dahlia.72570664.jpg"
+  }
+]);
